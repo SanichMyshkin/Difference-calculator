fc194adacf59c9d3498d1e7e59bcbd571fdcc3d4
Alexandr Myshkin <sir.myshkin@gmail.com>
2022-09-11 20:36:22 +0300

    First step

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -3,8 +3,12 @@ import argparse
 
 def gendiff():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
-    # parser = argparse.ArgumentParser(prog='gendiff')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     args = parser.parse_args()
     print(args)
+
+
+#poetry run gendiff -h
+#python3 -m pip install . gendiff
+#gendiff -h
