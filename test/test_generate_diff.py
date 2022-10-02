07e4437a3a231cf7043286f43e56294b3d7b85fe
Alexandr <sir.myshkin@gmail.com>
2022-10-03 01:12:10 +0300

    step 4 complete

--- test/test_generate_diff.py
+++ test/test_generate_diff.py
@@ -1,8 +1,11 @@
 from gendiff.engine import generate_diff
 
+file1_json = 'test/fixtures/file1.json'
+file2_json = 'test/fixtures/file2.json'
+
 
 def test_generate_diff():
     with open('test/fixtures/answer_json.txt', "r") as answer:
-        assert generate_diff('test/fixtures/file1.json', 'test/fixtures/file2.json') == answer.read()
+        assert generate_diff(file1_json, file2_json) == answer.read()
 
 # print(gendiff("/home/udo/python-project-50/test/fixtures/file1.json","/home/udo/python-project-50/test/fixtures/file2.json"))
