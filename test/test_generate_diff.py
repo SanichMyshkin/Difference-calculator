2ef45f03a94883fd33bf59d4a1df7e0868cc435c
Александр <sir.myshkin@gmail.com>
2022-11-01 00:38:49 +0300

    Filnaly. Project complate!

--- test/test_generate_diff.py
+++ test/test_generate_diff.py
@@ -1,33 +1,30 @@
 from gendiff.engine import generate_diff
 import pytest
+import os
 
-file1_json = 'test/fixtures/flat_file1.json'
-file2_yaml = 'test/fixtures/flat_file2.yaml'
+tree_file1_yaml = os.path.join(os.path.dirname(__file__), 'fixtures', 'tree_file1.yaml')
+tree_file2_json = os.path.join(os.path.dirname(__file__), 'fixtures', 'tree_file2.json')
 
-tree_file1_yaml = 'test/fixtures/tree_file1.yaml'
-tree_file2_json = 'test/fixtures/tree_file2.json'
+tree_answer = os.path.join(os.path.dirname(__file__), 'fixtures', 'answer_tree.txt')
+tree_answer_palin = os.path.join(os.path.dirname(__file__), 'fixtures', 'answer_tree_plain.txt')
+tree_answer_json = os.path.join(os.path.dirname(__file__), 'fixtures', 'answer_tree_json.txt')
 
 
 def test_parse():
     with pytest.raises(Exception):
-        generate_diff('test/fixtures/flat_file1.json', 'test/fixtures/file2.wrong')
-
-
-def test_generate_diff():
-    with open('test/fixtures/answer_flat.txt') as answer:
-        assert generate_diff(file1_json, file2_yaml) == answer.read()
+        generate_diff(tree_file1_yaml, "wrong_file")
 
 
 def test_gendiff_tree():
-    with open('test/fixtures/answer_tree.txt') as answer:
+    with open(tree_answer) as answer:
         assert generate_diff(tree_file1_yaml, tree_file2_json) == answer.read()
 
 
 def test_gendiff_plain():
-    with open('test/fixtures/answer_tree_plain.txt') as answer:
+    with open(tree_answer_palin) as answer:
         assert generate_diff(tree_file1_yaml, tree_file2_json, formater="plain") == answer.read()
 
 
 def test_gendiff_json():
-    with open('test/fixtures/answer_tree_json.txt') as answer:
+    with open(tree_answer_json) as answer:
         assert generate_diff(tree_file1_yaml, tree_file2_json, formater="json") == answer.read()
